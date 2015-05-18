266dda93c42ee5f684551dbf2cd0997359ed5553
Ryne Emory <ryneemory@gmail.com>
2015-05-18 16:16:56 -0600

    initial files and styles

---
+++ app.js
@@ -0,0 +1,10 @@
+var play = document.getElementsByClassName('fa-play')
+
+play.addEventListener ('click', function()
+
+)
+
+var food = document.getElementById('food')
+food.addEventListener('click', function()
+  
+)

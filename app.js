5a559df15e6f947205185765c553892c9b214421
Ryne Emory <ryneemory@gmail.com>
2015-05-19 11:36:10 -0600

    on off working

--- app.js
+++ app.js
@@ -1,29 +1,72 @@
-//defining variables to use for songs
-var food = document.getElementById('food'),
-    hunt = document.getElementById('hunt'),
-    blues = document.getElementById('blues'),
-    imfrom = document.getElementById('imfrom');
+var song1 = document.getElementById('song1'),
+    song2 = document.getElementById('song2'),
+    song3 = document.getElementById('song3'),
+    song4 = document.getElementById('song4');
 
 var stopSongs = function() {
-  food.pause();
-  hunt.pause();
-  blues.pause();
-  imfrom.pause();
+  song1.pause();
+  song2.pause();
+  song3.pause();
+  song4.pause();
 };
 
+var icon1 = document.getElementById('icon1'),
+    icon2 = document.getElementById('icon2'),
+    icon3 = document.getElementById('icon3'),
+    icon4 = document.getElementById('icon4');
 
-var stop1 = document.getElementById('stop1').style.visibility="hidden";
-
-
-//definining variables to use for icons
-var foods = document.getElementById('foods'),
-    hunts = document.getElementById('hunts'),
-    bluess = document.getElementById('bluess'),
-    imfroms = document.getElementById('imfroms');
-
-foods.addEventListener('click', function(){
+icon1.addEventListener('click', function(){
     stopSongs();
-    food.play();
-    this.style.visibility="hidden";
-    document.getElementById('stop1').style.visibility= "visible";
+    if (this.classList.contains('fa-play')) {
+      song1.play();
+      this.classList.remove('fa-play');
+      this.classList.add('fa-stop');
+    }
+    else {
+      song1.pause();
+      this.classList.remove('fa-stop');
+      this.classList.add('fa-play');
+    }
+});
+
+icon2.addEventListener('click', function(){
+  stopSongs();
+  if (this.classList.contains('fa-play')) {
+  song2.play();
+  this.classList.remove('fa-play');
+  this.classList.add('fa-stop');
+  }
+  else {
+    song2.pause();
+    this.classList.remove('fa-stop');
+    this.classList.add('fa-play');
+  }
+});
+
+icon3.addEventListener('click', function(){
+  stopSongs();
+  if (this.classList.contains('fa-play')) {
+    song3.play();
+    this.classList.remove('fa-play');
+    this.classList.add('fa-stop');
+  }
+  else {
+    song3.pause();
+    this.classList.remove('fa-stop');
+    this.classList.add('fa-play');
+  }
+});
+
+icon4.addEventListener('click', function(){
+  stopSongs();
+  if (this.classList.contains('fa-play')) {
+    song4.play();
+    this.classList.remove('fa-play');
+    this.classList.add('fa-stop');
+  }
+  else {
+    song4.pause();
+    this.classList.remove('fa-stop');
+    this.classList.add('fa-play');
+  }
 });

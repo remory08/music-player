4519b607bab77b244f9098a9d5375c1c914098a5
Ryne Emory <ryneemory@gmail.com>
2015-05-18 20:06:50 -0600

    divshot

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-var play = document.getElementsByClassName('fa-play')
+//defining variables to use for songs
+var food = document.getElementById('food'),
+    hunt = document.getElementById('hunt'),
+    blues = document.getElementById('blues'),
+    imfrom = document.getElementById('imfrom');
 
-play.addEventListener ('click', function()
+var stopSongs = function() {
+  food.pause();
+  hunt.pause();
+  blues.pause();
+  imfrom.pause();
+};
 
-)
+var playb = document.getElementsByClassName('fa-play');
+var stop = document.getElementById('stop').style.visibility="hidden";
 
-var food = document.getElementById('food')
-food.addEventListener('click', function()
-  
-)
+
+//definining variables to use for icons
+var foods = document.getElementById('foods'),
+    hunts = document.getElementById('hunts'),
+    bluess = document.getElementById('bluess'),
+    imfroms = document.getElementById('imfroms');
+
+foods.addEventListener('click', function(){
+    stopSongs();
+    food.play();
+    stop.style.visibility= "visible";
+    stop.addEventListener('click', function () {
+      food.pause();
+      this.style.visibility= "hidden";
+      playb.style.visibility= "visible";
+    })
+});
